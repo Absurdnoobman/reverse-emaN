2af0c31d81ce09cbdd4bdeea1407db454032fcc1
LadsForTea <88262542+Absurdnoobman@users.noreply.github.com>
2022-05-24 00:44:50 -0700

    Add files via upload

---
+++ This.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string eman[7] = {"Papa", "Alpha", "Oscar", "Tango", "Uniform", "November", "Golf"};
+	eman[3] = "T";
+	eman[0] = "P";
+	eman[1] = "A";
+	eman[2] = "O";
+	eman[4] = "U";
+	eman[6] = "G";
+	eman[5] = "N";
+	cout << eman[0] << eman[1] << eman[2] << eman[3] << eman[4] << eman[5] << eman[6];
+	return 0;
+}
